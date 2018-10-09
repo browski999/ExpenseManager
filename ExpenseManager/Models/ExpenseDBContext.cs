de352c7ea5dad94a72336884a16b21a715b45fdb
Dell <browski999@gmail.com>
2018-10-09 21:36:27 +0100

    Add project files.

---
+++ ExpenseManager/Models/ExpenseDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ExpenseManager.Models
+{
+    public class ExpenseDBContext : DbContext
+    {
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("ExpenseManagerDatabase"));
+            }
+        }
+    }
+}
